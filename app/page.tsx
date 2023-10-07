139dda3d93efd2b19d0c949ba152987ee0aa43e0
LoV432 <monib619@gmail.com>
2023-10-07 18:33:20 +0500

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -47,14 +47,14 @@ export default function Home() {
 		return `https://api.whatsapp.com/send?phone=${countryCode}${number}`;
 	}
 	return (
-		<div className="flex flex-col sm:flex-row gap-4">
+		<div className="flex flex-col gap-4 sm:flex-row">
 			<div className="h-12">
-				<details ref={dropdownRef} className="dropdown w-30 mb-32 z-10">
-					<summary ref={countrySlectorText} className="btn w-28 z-10">
+				<details ref={dropdownRef} className="w-30 dropdown z-10 mb-32">
+					<summary ref={countrySlectorText} className="btn z-10 w-28">
 						Select Country
 					</summary>
-					<input ref={searchTermRef} onKeyUp={(e) => hideLiElementsNotInSearch((e.target as HTMLInputElement).value)} placeholder="Search" className="input block input-bordered rounded rounded-b-none p-2 mt-2 ml-3.5 h-8 w-20" />
-					<ul ref={allCountriesRef} className="p-2 ml-3.5 shadow menu dropdown-content z-10 bg-base-100 rounded rounded-t-none w-20 h-fit max-h-56 overflow-y-scroll overflow-x-hidden flex-row">
+					<input ref={searchTermRef} onKeyUp={(e) => hideLiElementsNotInSearch((e.target as HTMLInputElement).value)} placeholder="Search" className="input input-bordered ml-3.5 mt-2 block h-8 w-20 rounded rounded-b-none p-2" />
+					<ul ref={allCountriesRef} className="menu dropdown-content z-10 ml-3.5 h-fit max-h-56 w-20 flex-row overflow-x-hidden overflow-y-scroll rounded rounded-t-none bg-base-100 p-2 shadow">
 						{codes.map((code) => (
 							<li onClick={() => updateCountryCode(code.countryCodes[0], code.isoCode2)} key={code.isoCode2}>
 								<a className="p-3">
@@ -64,11 +64,11 @@ export default function Home() {
 							</li>
 						))}
 					</ul>
-					<div className="w-screen h-screen fixed top-0 left-0 right-0 -z-40" onClick={closeDropDown}></div>
+					<div className="fixed left-0 right-0 top-0 -z-40 h-screen w-screen" onClick={closeDropDown}></div>
 				</details>
 			</div>
 			<input ref={numberRef} type="text" className="input input-bordered w-full" placeholder="Enter phone number" />
-			<button onClick={generateWhatsappLink} className="btn border-none hover:text-white active:text-white bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-500">
+			<button onClick={generateWhatsappLink} className="btn border-none bg-emerald-400 hover:bg-emerald-500 hover:text-white active:bg-emerald-500 active:text-white">
 				Chat!
 			</button>
 		</div>
