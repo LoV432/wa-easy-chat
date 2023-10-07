8af0bb40a59e38b5be76edd6f40135d9817792ba
LoV432 <monib619@gmail.com>
2023-10-07 19:55:54 +0500

    update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
 	title: 'Create WA Links',
-	description: 'Create WA links that lets you chat with number without adding them to contact'
+	description: 'Chat with WhatsApp numbers without the need to add them to your contacts'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="bg-[#128C7E]">
-			<body className="bg-[#128C7E] h-screen w-full flex justify-center place-items-center pb-32">{children}</body>
+			<body className="flex h-screen w-full place-items-center justify-center bg-[#128C7E] pb-32">{children}</body>
 		</html>
 	);
 }
