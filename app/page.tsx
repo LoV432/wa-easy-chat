5189ae7f222e3fdd3fb87e6f9192c612097618fc
LoV432 <monib619@gmail.com>
2023-10-11 19:08:14 +0500

    try script tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
 import ClientForm from './form.client';
 import { headers } from 'next/headers';
+import Script from 'next/script';
 
 export default function Home() {
 	const allHeaders = headers();
@@ -8,9 +8,7 @@ export default function Home() {
 	if (country === null) country = 'PK';
 	return (
 		<>
-			<Head>
-				<script defer data-domain="wa.monib.xyz" src="https://analytics-public.monib.xyz/js/script.tagged-events.js"></script>
-			</Head>
+			<Script defer data-domain="wa.monib.xyz" src="https://analytics-public.monib.xyz/js/script.tagged-events.js"></Script>
 			<div className="flex min-h-screen flex-col place-items-center justify-center gap-14">
 				<h1 className="text-center text-4xl font-bold sm:w-1/2">
 					Send WhatsApp Messages <br /> Hassle-Free
