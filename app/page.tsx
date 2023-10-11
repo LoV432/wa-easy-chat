5864c950ba593123e3ced4bca9a54522fca1f9b0
LoV432 <monib619@gmail.com>
2023-10-11 19:37:42 +0500

    add click goal + trigger click on enter key press

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,7 @@ export default function Home() {
 				<h1 className="text-center text-4xl font-bold sm:w-1/2">
 					Send WhatsApp Messages <br /> Hassle-Free
 				</h1>
-				<div className="flex flex-col gap-4 sm:flex-row">
-					<ClientForm initCountry={country} />
-				</div>
+				<ClientForm initCountry={country} />
 				<p className="w-10/12 text-center text-lg text-stone-950 sm:w-1/3 sm:text-base">Effortlessly send WhatsApp messages from your computer to any WhatsApp number without the need to add them as contacts. Our simple tool generates API links for seamless communication.</p>
 			</div>
 		</>
