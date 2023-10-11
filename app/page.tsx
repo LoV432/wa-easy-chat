3a9a37180180d27d8fd2a10cd8f4c15fd5589c96
LoV432 <monib619@gmail.com>
2023-10-11 19:08:14 +0500

    add analytics script tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 import ClientForm from './form.client';
 import { headers } from 'next/headers';
+import Script from 'next/script';
 
 export default function Home() {
 	const allHeaders = headers();
 	let country = allHeaders.get('x-vercel-ip-country');
 	if (country === null) country = 'PK';
 	return (
-		<div className="flex min-h-screen flex-col place-items-center justify-center gap-14">
-			<h1 className="text-center text-4xl font-bold sm:w-1/2">
-				Send WhatsApp Messages <br /> Hassle-Free
-			</h1>
-			<div className="flex flex-col gap-4 sm:flex-row">
-				<ClientForm initCountry={country} />
+		<>
+			<Script defer data-domain="wa.monib.xyz" src="https://analytics-public.monib.xyz/js/script.tagged-events.js"></Script>
+			<div className="flex min-h-screen flex-col place-items-center justify-center gap-14">
+				<h1 className="text-center text-4xl font-bold sm:w-1/2">
+					Send WhatsApp Messages <br /> Hassle-Free
+				</h1>
+				<div className="flex flex-col gap-4 sm:flex-row">
+					<ClientForm initCountry={country} />
+				</div>
+				<p className="w-10/12 text-center text-lg text-stone-950 sm:w-1/3 sm:text-base">Effortlessly send WhatsApp messages from your computer to any WhatsApp number without the need to add them as contacts. Our simple tool generates API links for seamless communication.</p>
 			</div>
-			<p className="w-10/12 text-center text-lg text-stone-950 sm:w-1/3 sm:text-base">Effortlessly send WhatsApp messages from your computer to any WhatsApp number without the need to add them as contacts. Our simple tool generates API links for seamless communication.</p>
-		</div>
+		</>
 	);
 }
